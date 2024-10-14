0610c9e61b422335028936b5f40a7d7a56757336
Truenno547YT <d.andres@correo.unimet.edu.ve>
2024-10-14 15:50:42 -0400

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # cubiculos_int
+
+Proyecto del equipo 5 de Ingenieria del Software
---
+++ android/app/src/main/kotlin/com/example/cubiculos_int/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.cubiculos_int
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
